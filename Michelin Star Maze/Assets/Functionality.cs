6424291a1da0874c094a73aefeb597a524c56d38
David Sellers <sturdy@1.1.1.1>
2020-11-02 16:46:49 -0500

    imported title

---
+++ Michelin Star Maze/Assets/Functionality.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Functionality : MonoBehaviour
+{
+   public void PlayGame () {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void BacktoMain ()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void OptionsMenu ()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
