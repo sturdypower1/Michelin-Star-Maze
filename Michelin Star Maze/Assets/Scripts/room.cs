41823eab56f6fae742ffca3cc3ced631d12196c6
David Sellers <sturdy@1.1.1.1>
2020-11-02 16:21:01 -0500

    started tutorial level

---
+++ Michelin Star Maze/Assets/Scripts/room.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class room : MonoBehaviour
+{
+    public bool doorNorth;
+    public bool doorEast;
+    public bool doorSouth;
+    public bool doorWest;
+
+}
