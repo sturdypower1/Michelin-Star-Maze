e7c3f03d0452d4ec8ca5c01c6420efbb665f1c25
David Sellers <sturdy@1.1.1.1>
2020-11-05 11:31:39 -0500

    test

--- Michelin Star Maze/Assets/Scripts/Movement.cs
+++ Michelin Star Maze/Assets/Scripts/Movement.cs
@@ -33,8 +33,8 @@ public class Movement : MonoBehaviour
 
         if(direction.magnitude >= 0.1f){
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            //float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            //transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir * speed * Time.deltaTime + (Time.deltaTime * moveDir * Input.GetAxis("Sprint") * (sprintSpeed -1 )));
