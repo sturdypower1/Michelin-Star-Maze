82a1d1d793c218d4d050191a761d0e4aa2a05fe7
David Sellers <sturdy@1.1.1.1>
2020-11-07 09:56:00 -0500

    started soundmanager

--- Michelin Star Maze/Assets/Scripts/Sound.cs
+++ Michelin Star Maze/Assets/Scripts/Sound.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class Sound : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public AudioSource audioSource;
+    public string soundName;
 }
---
+++ Michelin Star Maze/Assets/Scripts/soundManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public void playSound(){
+
+    }
+}
