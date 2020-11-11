aafba50f5c67fafd129677e4954e1e7f8affb7ee
David <cubsel@hotmail.com>
2020-11-11 10:48:52 -0500

    added audio

---
+++ Michelin Star Maze/Assets/Scripts/audioManager.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using UnityEngine;
+
+public class audioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    public static audioManager instance;
+
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+    private void Start()
+    {
+        if (!isPlaying("MenuMusic"))
+        {
+            play("MenuMusic");
+        }
+    }
+
+    public bool isPlaying(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.Log("name not found");
+            return false;
+        }
+        return s.source.isPlaying;
+    }
+
+    public void stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.Log("name not found");
+            return;
+        }
+        s.source.Stop();
+    }
+
+    public void play(string name){
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.Log("name not found");
+            return;
+        }
+        s.source.Play();
+    }
+
+    public void mute(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.Log("name not found");
+            return;
+        }
+        s.source.mute = true;
+    }
+    public void unmute(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.Log("name not found");
+            return;
+        }
+        s.source.mute = false;
+    }
+}
