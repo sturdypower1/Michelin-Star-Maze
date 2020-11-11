65971619f158bce9f07b4943487660d7825b9f02
David <cubsel@hotmail.com>
2020-11-11 13:42:18 -0500

    trying this

---
+++ Michelin Star Maze/Assets/Scripts/volumeControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class volumeControl : MonoBehaviour
+{
+    public void changeGlobalVolume(float volume)
+    {
+        audioManager AM = FindObjectOfType<audioManager>();
+        AM.setGlobalVolume(volume);
+    }
+}
